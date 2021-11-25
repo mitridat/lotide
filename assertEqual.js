b0d4a92495025e54944947b245941b5a944a2dc8
mitridat <don.lytov@gmail.com>
2021-11-25 03:46:36 +0000

    completed eqArrays.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,4 @@
-// FUNCTION IMPLEMENTATION
-// const sum = function(a, b) {
-//  return a + b;
-// };
-  
+// declares a function that compares actual to expected  
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log('Assertion Passed:' + actual + ' === ' + expected);
@@ -11,15 +7,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// assertEqual(eqArrays([1,2,3],[1,2,3]), true); // should return true
-// assertEqual(eqArrays([1,2,3],[1,"2",3]), true); //should return false
-// TEST CODE
-//  console.assert(sum(1, 2) === 3);
-//  console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+//  Testing specific combinations
 assertEqual(10, 10);
 assertEqual(10, 15);
 assertEqual('John', 'Walter');
 assertEqual('15', 15);
 assertEqual(sum(9, 6), 15);
-
-// random comments
--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,29 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log('Assertion Passed:' + actual + ' === ' + expected);
-    } else {
-      console.log('Assertion Failed:' + actual + ' !== ' + expected);
+// Defining the arrays to compare and the "array of arrays" containing all of them
+const array1 = [1, 2, 3];
+const array2 = [3, 2, 1];
+const array3 = ["1", "2", "3"];
+const array4 = ["1", "2", 3];
+const array0 = [array2, array3, array4];
+
+// loop this var from array1 to array4 as array0[i]
+// loop from first to last element of both arrays]
+const eqArrays = function(array1, expected) {
+  let equal = 1;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== expected[i]) {
+      equal = 0;
+      break;
     }
-  };
-  
-    assertEqual(10, 10);
-  assertEqual(10, 15);
-  assertEqual('John', 'Walter');
-  assertEqual('15', 15);
-  assertEqual(sum(9, 6), 15);
-  
+  }
+  if (equal === 1) {
+    return ('true:' + array1 + ' === ' + expected);
+  } else return ('false:' + array1 + ' === ' + expected);
+};
+
+for (let i = 0; i < array0.length; i++) console.log(eqArrays(array1, array0[i]));
+
+// Assignment: Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 // eqArrays([1, 2, 3], [1, 2, 3]) // => true
 // eqArrays([1, 2, 3], [3, 2, 1]) // => false
 // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false---------++++++++++++++++++++++++++++++++++++
