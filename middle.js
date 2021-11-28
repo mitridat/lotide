3953fad473b06d348a66f999e0a3feb46dce1d37
mitridat <don.lytov@gmail.com>
2021-11-28 02:38:48 +0000

    middle.js ready

--- middle.js
+++ middle.js
@@ -1,26 +1,12 @@
 // Array 1 exists
-var Array1 = [16, 18, 39, 24, 37, 263]
+let Array1 = [16, 18, 39, 24, 37, 263, 745, "ode"];
 
-// Create an empty Array2 - target of our function
-var Array2 = []
+let middle = function(arr) {
+  if (arr.length % 2 === 0) {
+    return `[${arr[arr.length / 2]}, ${arr[arr.length / 2 - 1]}]`;
+  } else {
+    return `[${arr[(arr.length - 1) / 2]}]`;
+  }
+};
 
-// count the number of elements in Array1
-var len = Array1.length
-
-// check what element will be the middle element; if the number of elements in array is odd, round it up
-var mid = Math.ceil(len)
-
-// checking parity of array length
-function(n) {
-    if len - mid
-    n = "even"
-    else n = "odd"
-}
-
-// filling Array2 with the middle elements of Array1; 1 if len is odd, or 2 if len is even
-// add to Array2 - Array1[mid-1]
-// then if n = "even"
-// add to Array2 - Array1[len-mid-1]
-// return Array2
-
-console.log(middle)
+console.log(middle(Array1));
