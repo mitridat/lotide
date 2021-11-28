851599b7f06b1db32bca887a776ae80a68c15cb6
mitridat <don.lytov@gmail.com>
2021-11-28 01:05:55 +0000

    without.js ready

---
+++ middle.js
@@ -0,0 +1,26 @@
+// Array 1 exists
+var Array1 = [16, 18, 39, 24, 37, 263]
+
+// Create an empty Array2 - target of our function
+var Array2 = []
+
+// count the number of elements in Array1
+var len = Array1.length
+
+// check what element will be the middle element; if the number of elements in array is odd, round it up
+var mid = Math.ceil(len)
+
+// checking parity of array length
+function(n) {
+    if len - mid
+    n = "even"
+    else n = "odd"
+}
+
+// filling Array2 with the middle elements of Array1; 1 if len is odd, or 2 if len is even
+// add to Array2 - Array1[mid-1]
+// then if n = "even"
+// add to Array2 - Array1[len-mid-1]
+// return Array2
+
+console.log(middle)
--- without.js
+++ without.js
@@ -1,8 +1,22 @@
-// Expected outcome
-// without([1, 2, 3], [1]) // => [2, 3]
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
-// Copy your assertArraysEqual and eqArrays functions into this new file.
-// Implement without which will return a subset of a given array, removing unwanted elements.
-// This function should take in a source array and a itemsToRemove array. 
-// It should return a new array with only those elements from source 
-// that are not present in the itemsToRemove array.
+/*
+Expected outcome
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+*/
+
+// Step 1: Array1 already exists
+const Array1 = [1, 2, 3, 4];
+const extra = 2;
+
+// Step 3: Loop function "without" cycling thru elements of Array 1
+let without = (Array1, extra) => {
+  // Step 4: Create an empty target Array2; remember that Array 1 should not be modified!
+  const Array2 = [];
+  for (let i = 0; i < Array1.length; i++)
+    if (Array1[i] !== extra)
+      Array2.push(Array1[i]);
+  return `[${Array1}], [${extra}] // => [${Array2}]`;
+};
+
+// Step 5: display the value
+console.log(without(Array1, extra));
